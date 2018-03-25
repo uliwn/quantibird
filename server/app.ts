a84845735a6a02bc52663e7b0b9e3228a0f945fa
Pauli Winkelmann <paw@ti8m.ch>
2018-03-25 19:45:25 +0200

    Add CORS support

--- server/app.ts
+++ server/app.ts
@@ -4,10 +4,15 @@ import * as express from 'express';
 import * as morgan from 'morgan';
 import * as mongoose from 'mongoose';
 import * as path from 'path';
+import * as cors from 'cors';
 
 import setRoutes from './routes';
 
 const app = express();
+
+// TODO disable for production!
+app.use(cors());
+
 dotenv.load({ path: '.env' });
 app.set('port', (process.env.PORT || 3000));
 
